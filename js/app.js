ab009d0d39df162221ed1afdba54cf22fdf73d8a
Jana Pascual <jacpascual@gmail.com>
2022-02-13 18:38:10 -0500

    Initialize shuffle function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 /** ----------------- Constants ----------------------- */
 deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 /** ----------------- Variables ------------------------*/
-let deck1, discardPile, turn, isWinner, cardsPicked, cardsToRemove 
+let discardPile, turn, isWinner, cardsPicked, cardsToRemove 
 let playerHand = []
 let dealerHand = []
-let shuffledCards = []
+let deck1 = [...deck]
+let shuffledDeck = []
+
 
 /** ------------- Cached Element References --------- */
 let yesBtn = document.querySelector("#yes-button")
@@ -28,18 +30,22 @@ function letsPlay() {
 }
 
 function init() {
-  deck1 = [...deck]    
+  shuffle()    
   initializePlayerHand()
   initializeDealerHand()
   playerRender()
   dealerRender()
 }
-// function shuffle(arr) {
+function shuffle() {
+  for (let i = 0; i < deck1.length; i++) {
+    shuffledDeck = deck1.splice(Math.random() * deck1.length, 1)
+    deck1.push({"faceUp": `${shuffledDeck}`})
+  }
   // shuffle deck1
     // Use for loop to iterate over game cards
       // randomly select a card to push into a new array for the length of deck1
       // push shuffled cards into array 
-//} 
+} 
 
 function pickRandCard() {
   let randCard = Math.floor(Math.random() * deck1.length, 1)
