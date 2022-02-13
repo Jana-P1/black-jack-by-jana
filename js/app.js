ac79a6cbd9f6f3d1928749f1e5be7865191f727f
Jana Pascual <jacpascual@gmail.com>
2022-02-13 11:26:41 -0500

    Start pseudocode

--- js/app.js
+++ js/app.js
@@ -14,3 +14,19 @@
 
 
 /** ------------------ Psuedo Code ------------------ */
+/** Start game function 
+ *    1. Upon opening the game, the player should only see h1, h2, and the yes/no buttons
+ *     2. Define state variables: deck, 
+ *     3. Define constants: card values => assign values to all cards in deck    
+ *     3. Define dealIn function that includes a handleClick
+ *          if player selects yes: 
+ *            invoke init() 
+ *            hide h1, h2, and yes/no buttons
+ *     4.  Define init function to initialize game:
+ *          - set deck variable to an array containing all cards (suit,           number/face)
+ *          - unhide all game-related elements
+ *          - invoke function to shuffle cards
+ *             - include setTimer function to simulate card shuffling with text informing the player that the cards are being shuffles
+ *  
+ *          - invoke render function
+ *          */
