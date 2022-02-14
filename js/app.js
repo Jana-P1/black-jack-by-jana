653a0b33babd229203b87fead1b0d930be71d7ed
Jana Pascual <jacpascual@gmail.com>
2022-02-14 15:44:23 -0500

    Assigns card values to deck

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 const deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 const player = 1
 const dealer = -1
-
+const cardValues = {
+  "dA": 1, "dQ": 10, "dK": 10, "dJ": 10,"d10": 10, "d09": 9, "d08": 8, "d07":7,"d06": 6, "d05": 5,"d04": 4,"d03": 3, "d02": 2, "hA": 1, "hQ": 10, "hK": 10, "hJ": 10, "h10": 10, "h09": 9, "h08": 8, "h07": 7, "h06": 6, "h05": 5, "h04": 4, "h03": 3, "h02": 2, "cA": 1, "cQ": 10, "cK": 10, "cJ": 10, "c10": 10, "c09": 9, "c08": 8, "c07": 7, "c06": 6, "c05": 5, "c04": 4, "c03": 3, "c02": 2, "sA": 1, "sQ": 10, "sK": 10, "sJ": 10, "s10": 10, "s09": 9, "s08": 8, "s07": 7, "s06": 6,"s05": 5, "s04": 4, "s03": 3, "s02": 2
+}
 /** ----------------- Variables ------------------------*/
 let discardPile, turn, isWinner, cardPicked, playerHand, dealerHand  
 let shuffledDeck = []
@@ -77,10 +79,15 @@ function initialRender() {
 }
 
 function pickACard() {
+
   render()
 }
 function render() {
   console.log("render invoked")
+}
+
+function findHandValues() {
+  console.log("find hand values function written")
 }
 
 
